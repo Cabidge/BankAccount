06ec8775ae912a349046716717cec0ef69328967
wdong20 <wdong20@stuy.edu>
2020-10-28 17:46:51 -0400

    Added private variables into BankAccount class and created a constructor. Created a BankAccount object inside Tester class with 20.20 balance, id 123, and password 'VeryGoodPassword'. Code compiles and runs.

--- BankAccount.java
+++ BankAccount.java
@@ -1,3 +1,11 @@
 public class BankAccount {
-    
+    private double balance;
+    private int accountID;
+    private String password;
+
+    public BankAccount(double bal, int id, String pass) {
+        balance = bal;
+        accountID = id;
+        password = pass;
+    }
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,5 @@
 public class Tester {
     public static void main(String[] ahhh) {
-
+        BankAccount account = new BankAccount(20.20, 123, "VeryGoodPassword");
     }
 }
