ba3f55a959e7ebd0d6aea89a607afa7cd6363015
wdong20 <wdong20@stuy.edu>
2020-11-01 13:55:26 -0500

    Tested transferTo method inside Tester class. Working as expected.

--- Tester.java
+++ Tester.java
@@ -2,11 +2,27 @@ public class Tester {
     public static void main(String[] ahhh) {
         BankAccount a = new BankAccount(2222, "VeryGoodPassword");
         BankAccount b = new BankAccount(2223, "GoodPassword");
+
         a.deposit(100);
         b.deposit(200);
-    }
 
-    public static void printAccountInfo(BankAccount account) {
-        System.out.print(account.toString());
+        System.out.println("The following should all be \"true\":");
+        System.out.println(a.getBalance() == 100);
+        System.out.println(b.getBalance() == 200);
+        System.out.println(a.transferTo(b, 100, "VeryGoodPassword"));
+        System.out.println(a.getBalance() == 0);
+        System.out.println(b.getBalance() == 300);
+        System.out.println(!a.transferTo(b, 100, "VeryGoodPassword"));
+        System.out.println(a.getBalance() == 0);
+        System.out.println(b.getBalance() == 300);
+        System.out.println(!b.transferTo(a, -100, "GoodPassword"));
+        System.out.println(a.getBalance() == 0);
+        System.out.println(b.getBalance() == 300);
+        System.out.println(b.transferTo(a, 150, "GoodPassword"));
+        System.out.println(a.getBalance() == 150);
+        System.out.println(b.getBalance() == 150);
+        System.out.println(!b.transferTo(a, 150, "BadPassword"));
+        System.out.println(a.getBalance() == 150);
+        System.out.println(b.getBalance() == 150);
     }
 }
